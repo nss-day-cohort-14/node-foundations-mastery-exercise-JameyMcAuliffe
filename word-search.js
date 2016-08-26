5eb15dbe018693334cab2309d8f5dd522bcbc248
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-08-26 09:55:06 -0500

    read stream created, word data shows in stdout

--- word-search.js
+++ word-search.js
@@ -1,1 +1,8 @@
-console.log('yo')
+"use strict";
+
+const { createReadStream } = require('fs')
+
+//variable to pipe data from words docs
+const readStream = createReadStream('/usr/share/dict/words')
+
+//readStream.pipe(process.stdout)
